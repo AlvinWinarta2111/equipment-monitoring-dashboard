5698690fe40155b4238f9b415cd6fa2e81e707df
Fndnvln <fernandonov123@gmail.com>
2025-09-03 15:23:44 +0800

    Update dashboard_pivot.py

--- dashboard_pivot.py
+++ dashboard_pivot.py
@@ -57,7 +57,7 @@ def main():
     with col1:
         st.image("https://raw.githubusercontent.com/AlvinWinarta2111/equipment-monitoring-dashboard/main/images/alamtri_logo.jpeg", width=200)
     with col2:
-        st.title("📊 Condition Monitoring Dashboard")
+        st.title("CHPP Condition Monitoring Dashboard")
     # --- End Logo and Title ---
 
     if 'clicked_trend_point' not in st.session_state:
